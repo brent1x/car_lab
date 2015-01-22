9d08a5a9ed0b03dc58068b02b7202da55f932df5
Brent Dady <brentdady@gmail.com>
2015-01-21 21:56:24 -0800

    partial solutions v1

--- src/car.js
+++ src/car.js
@@ -1,13 +1,54 @@
-function Car(make, model, year, color){
-
+function Car(make, model, year, color ) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.color = color;
+  this.passengers = [];
+  this.state = "off";
+  this.previous_owners = [];
+  this.current_owner = "Manufacturer";
 }
 
-Car.prototype.sale = function(newOwner){
+Car.prototype.sale = function(newOwner) {
+  this.previous_owners.push(this.current_owner);
+  this.current_owner = newOwner;
+};
+
+Car.prototype.paint = function(newColor) {
+  this.color = newColor;
+};
+
+Car.prototype.start = function() {
+  this.state = "on";
+};
+
+Car.prototype.off = function() {
+  this.state = "off";
+};
+
+Car.prototype.driveTo = function(destination) {
+  if (this.state === "on") {
+    console.log("driving to " + destination);
+  }
+};
+
+Car.prototype.park = function() {
+  if (this.state === "off") {
+    console.log("parked!!");
+  }
+};
+
+Car.prototype.pick_up = function(name) {
+  if (this.state === "on") {
+    this.passengers.push(name);
+    console.log("driving to pick up " + name);
+  } else {
+    this.passengers = [];
+  }
+};
+
+Car.prototype.dropOff = function(name) {
 
 };
 
-Car.prototype.paint = function(newColor){
-
-};
-
-module.exports=Car;
+module.exports = Car;
